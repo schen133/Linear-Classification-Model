ed97d7dbd9ead363613a5cf3d9a90bb66c67b437
Sifeng Chen <75231875+schen133@users.noreply.github.com>
2022-11-29 14:33:22 -0500

    Update readData.java

--- CSC242P4/src/learn/lc/examples/readData.java
+++ CSC242P4/src/learn/lc/examples/readData.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 //Required testing files:
 //earthquake clean
 //earthquake noisy
 //house votes numerical version
 public class readData {
-
     //for Earthquake data:
     //        6.1,4.9,0
     //        5.3,4.4,1
@@ -28,20 +28,89 @@ public class readData {
         Scanner scnr = new Scanner(new File(filename));
 
         while(scnr.hasNext()){
-        Example ex = new Example(3);
+        //create new Example instance called exam
+        Example exam = new Example(3);
+        String input = scnr.nextLine();
+        String[] doubArray = input.split(",");
+
+        //sets exam's input array and output
+        exam.inputs[0] = 1.0;
+        //first double of the input from file
+        exam.inputs[1] = Double.parseDouble(doubArray[0]);
+        //second double of the input
+        exam.inputs[2] = Double.parseDouble(doubArray[1]);
+        //third double of the input
+        exam.output = Double.parseDouble(doubArray[2]);
         
-
-
+        //add example to arrayList of examples
+        arr.add(exam);
         }
+
         return arr;
+    }
+
+    public static ArrayList<Example> readEarthOrHouseData(String filename) throws FileNotFoundException {
+
+        ArrayList<Example> arr = new ArrayList<Example>();
+        Scanner scnr = new Scanner(new File(filename));
+
+        while(scnr.hasNext()){
+            //create new Example instance called exam
+            String input = scnr.nextLine();
+            
+            //make an input array that separates input string by , 
+            String[] doubArray = input.split(",");
+
+            //create new exam for each line of data file, exam's input array size will be the length, because 
+            //although it should be length - 1 because output is not in the input array, but since later
+            //we will have to add x_0 = 1.0 to exam's input array anyway. It's the same size still
+            Example exam = new Example(doubArray.length);
+
+            //For each line in the earth data file, we set exam's first index to 1.0
+            //x_0 = 1.0 always
+            exam.inputs[0] = 1.0;
+
+            //then for the rest of the exam's array, we will read through the input array (doubArray)
+            //goes through every line from start till the output value
+            for(int i=0; i<doubArray.length-1; i++) {
+            //first double of the input from file
+            exam.inputs[i+1] = Double.parseDouble(doubArray[i]);
+            //second double of the input
+            //third double of the input
+            }
+            
+            //output is the end of the doubleArray
+            exam.output = Double.parseDouble(doubArray[doubArray.length-1]);
+
+            //add example to arrayList of examples
+            arr.add(exam);
+
+            }
+
+        return arr;
+        
+    }
+
+    public static void printArrayOfExamples(ArrayList<Example> array) {
+    
+    for(Example e: array) {
+    System.out.println(e);
+    }
 
     }
 
-    public static ArrayList<Example> readHouseData(String filename) {
-        ArrayList<Example> arr = new ArrayList<Example>();
+    public static void main(String[] args) throws FileNotFoundException {
+    
+    // ArrayList<Example> array = readEarthData("earthquake-clean.data.txt");
 
-        return arr;
-        
+    ArrayList<Example> arrForEarth = readEarthOrHouseData("earthquake-clean.data.txt");
+
+    ArrayList<Example> arrForHouse = readEarthOrHouseData("house-votes-84.data.num.txt");
+
+    printArrayOfExamples(arrForEarth);
+    // System.out.println("GAPPP");
+    printArrayOfExamples(arrForHouse);
+
     }
 
 
