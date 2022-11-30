079cd0f40c7b9f096e9989241f842d6abe9266c7
Sifeng Chen <75231875+schen133@users.noreply.github.com>
2022-11-30 12:57:39 -0500

    Update LinearClassifier.java
    Uncomment printing statement

--- CSC242P4/src/learn/lc/core/LinearClassifier.java
+++ CSC242P4/src/learn/lc/core/LinearClassifier.java
@@ -71,7 +71,7 @@ abstract public class LinearClassifier {
 	 * Subclasses can override it to gather statistics or update displays.
 	 */
 	protected void trainingReport(List<Example> examples, int stepnum, int nsteps) {
-		//System.out.println(stepnum + "\t" + accuracy(examples));
+		System.out.println(stepnum + "\t" + accuracy(examples));
 	}
 	
 	/**
